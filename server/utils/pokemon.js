049b5d35993a852c6cb324614e59810203db467d
Sugiyama-KM <takanobu.sugiyama@konicaminolta.com>
2024-02-28 19:44:15 +0900

    Add logs

--- server/utils/pokemon.js
+++ server/utils/pokemon.js
@@ -6,6 +6,7 @@ const agent = new ProxyAgent();
 export const findPokemon = async (name) => {
   try{
     console.log("start findPokemon");
+    console.log(name);
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`,{agent});
     const pokemon = await response.json();
     return pokemon;
