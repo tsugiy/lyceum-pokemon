d00eeee760f95363a5d5da9de31cd3a10ba60bcd
Sugiyama-KM <takanobu.sugiyama@konicaminolta.com>
2024-02-28 18:23:55 +0900

    Change exception log

--- server/utils/pokemon.js
+++ server/utils/pokemon.js
@@ -4,21 +4,11 @@ const agent = new ProxyAgent();
 
 /** ポケモンの取得 */
 export const findPokemon = async (name) => {
-  /*
-  await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
-      method: 'GET'
-    })
-    .then(response => {
-      if (!response.ok) {
-        console.error('サーバーエラー');
-      }
-    })
-    .catch(error => {
-      console.error('通信に失敗しました', error);
-  });
-  */  
-
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
-  const pokemon = await response.json();
-  return pokemon;
+  try{
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`,{agent});
+    const pokemon = await response.json();
+    return pokemon;
+  } catch (error) {
+    console.log(error);
+  }
 };
