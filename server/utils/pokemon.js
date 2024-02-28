cd6cd512855a155c44712b2bf9a1e3f9f1204af8
Sugiyama-KM <takanobu.sugiyama@konicaminolta.com>
2024-02-28 13:33:42 +0900

    Add setting proxy-agent

--- server/utils/pokemon.js
+++ server/utils/pokemon.js
@@ -1,7 +1,11 @@
+import { ProxyAgent } from "proxy-agent";
+ 
+const agent = new ProxyAgent();
+
 /** ポケモンの取得 */
 export const findPokemon = async (name) => {
-  
-  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
+  /*
+  await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, {
       method: 'GET'
     })
     .then(response => {
@@ -12,9 +16,9 @@ export const findPokemon = async (name) => {
     .catch(error => {
       console.error('通信に失敗しました', error);
   });
-  
+  */  
 
-  //const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+  const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
   const pokemon = await response.json();
   return pokemon;
 };
